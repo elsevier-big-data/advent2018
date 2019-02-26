7d2dbd0ad4b8a2bbeb64f44b9a2a66c8f1af6e2f
Emma Burrows <e.burrows@elsevier.com>
2019-02-26 13:52:28 +0000

    Dawkins weasel

--- src/dawkinsweasel.clj
+++ src/dawkinsweasel.clj
@@ -12,17 +12,42 @@
 
 (def random-population #(take 100 (repeatedly random-individual)))
 
-
 (defn compare-individuals
-  [x y]
-  (count (filter (fn [[a b]] (= a b)) (map vector x y))))
-
-
-(defn compare-individuals2
   [x y]
   (->> (map vector x y)
        (filter (fn [[a b]] (= a b)))
        count))
 
+(defn fitness
+  [s]
+  (compare-individuals s "METHINKS IT IS LIKE A WEASEL"))
 
+(defn fittest
+  [xs]
+  (last (sort-by fitness xs)))
 
+(defn mutate-gene
+  [c]
+  (if (< (rand) 0.05)
+    (rand-nth genes)
+    c))
+
+(defn mutate
+  [s]
+  (apply str (map mutate-gene s)))
+
+(defn reproduce
+  [s]
+  (map mutate (take 100 (repeatedly (constantly s)))))
+
+(defn next-generation
+  [pop]
+  (println (fittest pop))
+  (reproduce (fittest pop)))
+
+(defn dawkinsweasel
+  [pop threshold]
+  (first
+    (drop-while
+    #(not= (-> %1 fittest fitness) threshold)
+    (iterate next-generation pop))))
